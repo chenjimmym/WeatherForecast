cb756a2dd71b639e6e4c1fb4c7caeaefae2c6ed1
Jimmy <chen.jimmym@gmail.com>
2017-09-29 21:45:41 -0700

    done

--- weather.js
+++ weather.js
@@ -8,8 +8,14 @@ $(document).ready(function() {
 
         var city = $("input[name=city]").val();
         alert(city);
-        $.get(url, function(res) {
+        $.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=9b8fdd801549a1d8123c9aa00e51a3d0`, function(result) {
             // your code here
+            console.log(result);
+            var fah = Math.floor((9/5)*(result.main.temp-273)+32);
+            $('#container').append(`
+                <h2>${result.name}</h2>
+                <h3>Temperature: ${fah}</h3>
+            `);
         }, 'json');
         // don't forget to return false so the page doesn't refresh
         return false;
